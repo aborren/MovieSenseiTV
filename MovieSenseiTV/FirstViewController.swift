d9e0325ce5225e4e88a6e8efd54c1afef6c2d5d2
Dan Isacson <dan_isacson@hotmail.com>
2015-11-20 20:28:57 +0100

    Initial Commit

---
+++ MovieSenseiTV/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MovieSenseiTV
+//
+//  Created by Dan Isacson on 20/11/15.
+//  Copyright © 2015 dna. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MovieSenseiTV/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MovieSenseiTV
+//
+//  Created by Dan Isacson on 20/11/15.
+//  Copyright © 2015 dna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
